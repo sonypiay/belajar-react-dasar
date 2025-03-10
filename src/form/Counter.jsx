aa47437935898b7720e65950b54e81baec2ba8b0
sonypiay <pyscho30@gmail.com>
2025-03-10 23:25:15 +0700

    state updates

--- src/form/Counter.jsx
+++ src/form/Counter.jsx
@@ -3,8 +3,11 @@ import {useState} from "react";
 export default function Counter() {
     let [counter, setCounter] = useState(0);
 
+    console.log(`Render counter: ${counter}`);
+
     function handlerClick() {
-        setCounter(counter + 1);
+        // setCounter(counter + 1);
+        setCounter(c => c + 1);
         console.log(counter);
     }
 
